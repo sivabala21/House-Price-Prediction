720264f4fe6c608f65fff3c52f44baff52eb17b8
sivabala21 <sivabalahack@gmail.com>
2024-08-12 12:36:34 +0530

    dollar to ruppee

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
     prediction = model.predict(final_features)
     output = round(prediction[0], 2)
     
-    return render_template('index.html', prediction_text=f'Predicted House Price: ${output}')
+    return render_template('index.html', prediction_text=f'Predicted House Price: ₹{output}')
 
 if __name__ == "__main__":
     app.run(debug=True)
